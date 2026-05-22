dafc5d98be2a6b467c96090a4143ada9855e0616
Andika Nugraha <andikanugraha1802@gmail.com>
2026-05-22 14:04:05 +0800

    Membuat CRUD Customers

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('services', ServiceController::class);
 Route::patch('services/{service}/activate', [ServiceController::class, 'activate'])->name('services.activate');
 Route::patch('services/{service}/deactivate', [ServiceController::class, 'deactivate'])->name('services.deactivate');
+
+Route::apiResource('customers', CustomerController::class);
+Route::patch('customers/{customer}/activate', [CustomerController::class, 'activate'])->name('customers.activate');
+Route::patch('customers/{customer}/deactivate', [CustomerController::class, 'deactivate'])->name('customers.deactivate');
 
 
 ?>
