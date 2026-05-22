c394b8c269a98b1555789e7d1d880b075de23d7e
Andika Nugraha <andikanugraha1802@gmail.com>
2026-05-22 13:44:09 +0800

    Membuat model dan migration customers

---
+++ app/Models/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'customer_id',
+        'name',
+        'email',
+        'phone',
+        'address',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => 'boolean',
+        ];
+    }
+}
